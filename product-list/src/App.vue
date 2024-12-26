b8fe9ea2e055aca0a982294cd9bb8d9cfe431855
ipz06 <it.vania.9@gmail.com>
2024-12-26 17:23:22 +0200

    Add vue

---
+++ product-list/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import CartLayout from './components/CartLayout.vue';
+import DessertsLayout from './components/DessertsLayout.vue';
+
+</script>
+
+<template>
+    
+  <main>
+   <DessertsLayout />
+   <CartLayout />
+  </main>
+</template>
+
+<style scoped>
+
+</style>

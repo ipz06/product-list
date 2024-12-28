4346fb1067a980b131bd9de29f30cf6d7d70ad59
ipz06 <it.vania.9@gmail.com>
2024-12-28 14:26:05 +0200

    Add type CartItems

---
+++ product-list/src/types/CartItems.ts
@@ -0,0 +1,12 @@
+export interface CartItem {
+    id: string;
+    quantity: number;
+    name?: string;
+    price: number;
+    thumbnail?: string;
+    // image?: {
+    //     desktop: string,
+    //     mobile: string,
+    //     thumbnail: string,
+    // },
+  }
